4b588a9e303c47229a79d5a43b7c0b03c3c3a095
Alexander Chertovsky <kygechukzp@gmail.com>
2015-12-18 12:19:01 +0200

    fixed migration issue

--- migrations/m151218_095432_rename_errors_field.php
+++ migrations/m151218_095432_rename_errors_field.php
@@ -17,7 +17,7 @@ class m151218_095432_rename_errors_field extends Migration
     {
         if (!empty($this->db->getTableSchema('{{%syslog}}')->getColumn('issues'))) {
             $this->dropColumn('{{%syslog}}', 'issues');
-            $this->addColumn('{{%syslog}}', 'error_json', $this->text());
+            $this->addColumn('{{%syslog}}', 'errors_json', $this->text());
         }
     }
 }
